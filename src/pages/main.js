ceb4d564865346f5d44076d65ec0d5c99c66e37c
Alok Sahu <tusharsahu2427@gmail.com>
2023-10-18 22:25:39 +0530

    v1.2

--- src/pages/main.js
+++ src/pages/main.js
@@ -13,13 +13,13 @@ const Main = () => {
             <div className="container-fluid mt-3">
 
                 <nav className="navbar navbar-expand-lg navbar-light bg-white shadow-md">
-                    <div className="container-fluid p-2">
+                    <div className="container-fluid p-2 header">
 
                         <div className="btn btn-primary iconOuter" onClick={ToggleSidebar}  >
                             <i class="gg-menu"></i>
                         </div>
 
-                        <a className="navbar-brand text-primary mr-0"><i class="gg-profile" onClick={() => { setProfile(!openProfile) }}></i></a>
+                        <a className="navbar-brand text-primary mr-0"><i class="gg-profile" onClick={() => { setProfile(!openProfile) }} style={{ cursor: "pointer" }}></i></a>
                         {openProfile &&
                             <div className="more-profile">
                                 <li>More Info </li>
@@ -36,22 +36,81 @@ const Main = () => {
                     </div>
                     <div className="sd-body">
                         <ul>
-                            <li><a className="sd-link">Menu Item 1</a></li>
-                            <li><a className="sd-link">Menu Item 2</a></li>
-                            <li><a className="sd-link">Menu Item 3</a></li>
-                            <li><a className="sd-link">Menu Item 4</a></li>
-                            <li><a className="sd-link">Menu Item 5</a></li>
-                            <li><a className="sd-link">Menu Item 6</a></li>
-                            <li><a className="sd-link">Menu Item 7</a></li>
-                            <li><a className="sd-link">Menu Item 8</a></li>
+                            <DropLi
+                                nav='NavItem 1'
+                                navItems={[
+                                    "Item 1",
+                                    "item 2",
+                                    "item 3"
+                                ]}
+                            />
+                            <li className="sd-link" >Item 2</li>
+                            <li className="sd-link" >Item 3</li>
+                            <li className="sd-link" >Item 4</li>
+                            <li className="sd-link" >Item 5</li>
+
                         </ul>
                     </div>
                 </div>
                 <div className={`sidebar-overlay ${isOpen == true ? 'active' : ''}`} onClick={ToggleSidebar}></div>
             </div>
 
+            <div className="container card-container">
+                <ItemCard/>
+                <ItemCard/>
+
+                <ItemCard/>
+                <ItemCard/>
+                <ItemCard/>
+                <ItemCard/>
+            </div>
         </>
     )
 }
 
 export default Main
+
+
+
+const DropLi = ({ nav, navItems }) => {
+
+    const [isNavOpen, setIsNavOpen] = useState(false)
+
+    return (
+        <>
+
+            <li onClick={() => setIsNavOpen(!isNavOpen)} className="sd-link">{nav}</li>
+            {
+                isNavOpen && navItems.length > 0 && navItems.map((item, i) => {
+                    return (
+                        <div key={i} className="navbarLi">
+
+                            <li className="sd-link" style={{ paddingLeft: '30px' }}>{item}</li>
+                        </div>
+                    )
+                })
+            }
+
+
+        </>
+    )
+}
+
+
+
+const ItemCard = () => {
+    return (
+        <>
+            <div class="product-card">
+                <div class="product-img img-three"></div>
+                <div class="product-text">
+                    <h3>Madrid</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, laborum. Non maxime corrupti illo necessitatibus error vitae numquam perspiciatis culpa.</p>
+                </div>
+                <div class="product-cart">
+                    <button type="submit">Add to cart</button>
+                </div>
+            </div>
+        </>
+    )
+}
