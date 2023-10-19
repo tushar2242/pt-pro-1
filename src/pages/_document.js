ffdad99faf968378ec42896e6221eacbf6a097be
Alok Sahu <tusharsahu2427@gmail.com>
2023-10-19 20:45:46 +0530

    v1.4.2.1

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,13 +5,12 @@ export default function Document() {
     <Html lang="en">
 
       <Head>
-        <link
-          href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.16.0/font/bootstrap-icons.css"
-          rel="stylesheet"
-        />
+       <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css' />
 
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
         <script src="https://kit.fontawesome.com/d90ef4704f.js" crossorigin="anonymous"></script>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
       </Head>
       <body>
         <Main />
